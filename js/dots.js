189e0937ce991c794026469f65855790113f04e4
Zirak <zirakertan@gmail.com>
2022-08-01 12:40:26 +0000

    refactor: Clean the dots script up

--- js/dots.js
+++ js/dots.js
@@ -1,80 +1,75 @@
 /*global setTimeout*/
-var canvas = document.getElementById('dots'),
-    ctx = canvas.getContext('2d');
+const canvas = document.getElementById('dots');
+const ctx = canvas.getContext('2d');
 
-function resizeCanvas () {
-	var bbox = document.body.getBoundingClientRect();
-	canvas.height = bbox.height;
-	canvas.width = bbox.width;
+function resizeCanvas() {
+    const { height, width } = document.body.getBoundingClientRect();
+    canvas.height = height;
+    canvas.width = width;
 }
 window.addEventListener('load', resizeCanvas);
 window.addEventListener('resize', resizeCanvas);
 
-var dots = [];
-var Dot = {
-	draw : function (dot, ctx) {
-		ctx.beginPath();
-		ctx.arc(dot.x, dot.y, dot.radius, 0, Math.PI*2, true);
-		ctx.closePath();
+let dots = [];
+const Dot = {
+    draw(dot, ctx) {
+        ctx.beginPath();
+        ctx.arc(dot.x, dot.y, dot.radius, 0, Math.PI * 2, true);
+        ctx.closePath();
 
-		ctx.fillStyle = dot.colour;
-		ctx.fill();
-	},
+        ctx.fillStyle = dot.colour;
+        ctx.fill();
+    },
 
-	step : function (dot) {
-		var origDelta;
+    step(dot) {
+        if (dot.radius >= dot.maxRadius && !dot.killTimeout) {
+            let origDelta = dot.radiusDelta;
+            dot.radiusDelta = 0;
 
-		if (dot.radius >= dot.maxRadius && !dot.killTimeout) {
-			origDelta = dot.radiusDelta;
-			dot.radiusDelta = 0;
+            dot.killTimeout = setTimeout(() => {
+                dot.radiusDelta = origDelta * -1;
+            }, 1500 / dots.length);
+        }
 
-			dot.killTimeout = setTimeout(function () {
-				dot.radiusDelta = origDelta * -1;
-			}, 1500 * 1/dots.length);
-		}
-
-		dot.radius += dot.radiusDelta;
-	}
+        dot.radius += dot.radiusDelta;
+    },
 };
 
-setTimeout(function addDotInterval () {
+setTimeout(function addDotInterval() {
     addDot();
-	setTimeout(addDotInterval, 3000);
+    setTimeout(addDotInterval, 3000);
 });
 
-setTimeout(function render () {
-	setTimeout(render, 60);
+setTimeout(function render() {
+    setTimeout(render, 60);
 
-	dots = dots.filter(function (dot) {
-		return dot.radius > 0;
-	});
+    dots = dots.filter((dot) => dot.radius > 0);
 
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-	dots.forEach(function (dot) {
-		Dot.draw(dot, ctx);
-		Dot.step(dot);
-	});
+    dots.forEach((dot) => {
+        Dot.draw(dot, ctx);
+        Dot.step(dot);
+    });
 });
 
-function addDot () {
+function addDot() {
     dots.push({
-        x : Math.random() * canvas.width,
-        y : Math.random() * canvas.height,
-        colour : randomColour(),
+        x: Math.random() * canvas.width,
+        y: Math.random() * canvas.height,
+        colour: randomRGBA(),
 
-		radius : 1,
-		maxRadius : Math.random() * 7 + 20,
-		radiusDelta : Math.random() + 0.1
+        radius: 1,
+        maxRadius: Math.random() * 7 + 20,
+        radiusDelta: Math.random() + 0.1,
     });
 
-    function randomColour () {
-        return 'rgba(' +
-			[randColour(), randColour(), randColour()].join(', ') +
-			', 0.1)';
-
+    function randomRGBA() {
+        const rgb = [randomColour(), randomColour(), randomColour()];
+        return `rgba(${rgb}, 0.1)`;
     }
-    function randColour () {
-        return Math.floor(Math.random()*0xff);
+
+    function randomColour() {
+        return Math.floor(Math.random() * 0xff);
     }
 }
